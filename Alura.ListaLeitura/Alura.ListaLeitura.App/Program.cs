56905a923233648d627c6fa7607bd0ad62fc1684
Alan <alanbritodossantos2015@gmail.com>
2021-12-18 23:16:37 -0300

    Melhorando a lógica de tratamento de requisições

---
+++ Alura.ListaLeitura/Alura.ListaLeitura.App/Program.cs
@@ -0,0 +1,30 @@
+﻿using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace Alura.ListaLeitura.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _repo = new LivroRepositorioCSV();
+
+            IWebHost host = new WebHostBuilder()
+                .UseKestrel() //servidor escolhido
+                .UseStartup<Startup>()// classe que inicializa o host  -- o codigo de configuração vai ficar nessa classe
+                .Build();
+
+            host.Run();
+            //ImprimeLista(_repo.ParaLer);
+            //ImprimeLista(_repo.Lendo);
+            //ImprimeLista(_repo.Lidos);
+        }
+
+        static void ImprimeLista(ListaDeLeitura lista)
+        {
+            Console.WriteLine(lista);
+        }
+    }
+}
---
+++ Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,54 @@
+﻿using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(Roteamento);
+        }
+
+        public Task Roteamento(HttpContext context)
+        { 
+            var _repo = new LivroRepositorioCSV();
+            var caminhosAtendidos = new Dictionary<string, RequestDelegate> {
+                {"/Livros/ParaLer", LivrosParaLer },
+                {"/Livros/Lendo", LivrosLendo },
+                {"/Livros/Lidos", LivrosLidos }
+
+            };
+
+            if (caminhosAtendidos.ContainsKey(context.Request.Path))
+            {
+                var metodo = caminhosAtendidos[context.Request.Path];
+                return metodo.Invoke(context);
+            }
+
+            context.Response.StatusCode = 404;
+            return context.Response.WriteAsync("Caminho inexistente.");
+        }
+
+        public Task LivrosParaLer(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.ParaLer.ToString());
+        }
+
+        public Task LivrosLendo(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.Lendo.ToString());
+        }
+
+        public Task LivrosLidos(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.Lidos.ToString());
+        }
+    }
+}
