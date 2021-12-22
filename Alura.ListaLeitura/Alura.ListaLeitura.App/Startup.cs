a763345ac7eeb3c705992de538573dec5b532bcc
Alan <alanbritodossantos2015@gmail.com>
2021-12-22 03:12:03 -0300

    foi criado um arquivo html para exibir o front

--- Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -39,7 +40,7 @@ namespace Alura.ListaLeitura.App
         {
             var livro = new Livro()
             {
-                Titulo = context.Request.Query["titulo"].First(),//pega o primeiro valor de estiver no link
+                Titulo = context.Request.Query["titulo"].First(),//pega o primeiro valor que estiver no link
                 Autor = context.Request.Query["autor"].First(),
             };
             var repo = new LivroRepositorioCSV();
@@ -50,15 +51,17 @@ namespace Alura.ListaLeitura.App
         private Task ExibeFormulario(HttpContext context)
         {
             //O sinal "@" permite escrever mais de uma linha
-            var html = @"
-                    <html>
-                        <form action='/Cadastro/Incluir'>
-                            <input name='titulo' />
-                            <input name='autor' />
-                            <button>Gravar</button>
-                        </form>
-                    </html>";
+            var html = CarregaArquivoHTML("formulario");
             return context.Response.WriteAsync(html);
+        }
+
+        private string CarregaArquivoHTML(string nomeArquivo)
+        {
+            var nomeCompletoArquivo = $"HTML/{nomeArquivo}.html";
+            using (var arquivo = File.OpenText(nomeCompletoArquivo))
+            {
+                return arquivo.ReadToEnd();//vai ler todo o conteudo e coloca numa string
+            }
         }
 
         private Task ExibeDetalhes(HttpContext context)
